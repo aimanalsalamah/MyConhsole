7d5152bc71bc155186e38922eb7e376b0505a432
Faisal Turki <SRG1@RY85862-LAP6>
2022-07-18 11:10:54 +0300

    hhh

--- MyConhsole/Program.cs
+++ MyConhsole/Program.cs
@@ -1,56 +1,87 @@
-﻿line1:
+﻿//This is Faisal Branch Welcome
+line1:
 Console.WriteLine("Enter what you need to do:");
-var job = Console.ReadLine();
-if (job == "+")
+
+switch (Console.ReadLine())
 {
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 + value2;
-    Console.WriteLine("Value1 + value2 = " + result);
-}
-if (job == "-")
-{
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 - value2;
-    Console.WriteLine("Value 1 - value 2 = " + result);
-}
-if (job == "/")
-{
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 / value2;
-    Console.WriteLine("Value1 / value2 = " + result);
-}
-if (job == "*")
-{
-    Console.WriteLine("Enter value1");
-    var value1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter value2");
-    var value2 = int.Parse(Console.ReadLine());
-    var result = value1 * value2;
-    Console.WriteLine("Value 1 * value 2 = " + result);
-}
-if(job == "text")
-{
-    Console.WriteLine("Do you want to read or write");
-    var selection = Console.ReadLine();
-    if (selection == "read")
-    {
-        var noteText = System.IO.File.ReadAllText("Note.txt");
-        Console.WriteLine(noteText);
-    }
-    if (selection == "write")
-    {
-        Console.WriteLine("Please enter your note");
-        var entry = Console.ReadLine();
-        System.IO.File.AppendAllText("Note.txt", entry + "\n");
-    }
+    case "data":
+        foreach (var item in Tools.JsonFunctions.DeserializeJson<List<Users>>("Data"))
+        {
+            Console.WriteLine($"name:{item.name}, Age:{item.Age}, Mobile:{item.Mobile}");
+        }
+        break;
+    case "customer":
+        foreach (var item in Tools.JsonFunctions.DeserializeJson<List<Customers>>("Customers"))
+        {
+            Console.WriteLine($"name:{item.name}, Age:{item.Age}");
+        }
+        break;
+    case "+":
+        Console.WriteLine("Enter value1");
+        var value1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value2 = int.Parse(Console.ReadLine());
+        var result = value1 + value2;
+        Console.WriteLine("Value1 + value2 = " + result);
+        break;
+    case "-":
+        Console.WriteLine("Enter value1");
+        var value3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value4 = int.Parse(Console.ReadLine());
+        var result1 = value3 - value4;
+        Console.WriteLine("Value 1 - value 2 = " + result1);
+        break;
+    case "/":
+        Console.WriteLine("Enter value1");
+        var value5 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value6 = int.Parse(Console.ReadLine());
+        var result2 = value5 / value6;
+        Console.WriteLine("Value1 / value2 = " + result2);
+        break;
+    case "*":
+        Console.WriteLine("Enter value1");
+        var value7 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter value2");
+        var value8 = int.Parse(Console.ReadLine());
+        var result3 = value7 * value8;
+        Console.WriteLine("Value 1 * value 2 = " + result3);
+        break;
+    case "text":
+        Console.WriteLine("Do you want to read or write");
+        var selection = Console.ReadLine();
+        if (selection == "read")
+        {
+            var noteText = System.IO.File.ReadAllText("Note.txt");
+            Console.WriteLine(noteText);
+        }
+        if (selection == "write")
+        {
+            Console.WriteLine("Please enter your note");
+            var entry = Console.ReadLine();
+            System.IO.File.AppendAllText("Note.txt", entry + "\n");
+        }
+        break;
+    default:
+        Console.WriteLine("Wrong command.");
+        break;
 }
 goto line1;
+
+public class Users
+{
+    public string name { get; set; }
+    public int Age { get; set; }
+    public string Mobile { get; set; }
+    public string Email { get; set; }
+    public string MaritalStatus { get; set; }
+    public int Children { get; set; }
+    public string City { get; set; }
+    public string Area { get; set; }
+}
+public class Customers
+{
+    public string name { get; set; }
+    public int Age { get; set; }
+}
---
+++ Tools/JsonFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools
+{
+    public class JsonFunctions
+    {
+        public static T DeserializeJson<T>(string fileName) where T : new()
+        {
+            if (System.IO.File.Exists(@$"..\..\..\{fileName}.json"))
+            {
+                var json = System.IO.File.ReadAllText(@$"..\..\..\{fileName}.json");
+                return System.Text.Json.JsonSerializer.Deserialize<T>(json);
+            }
+            else
+            {
+                Console.WriteLine($"The file {fileName}.json not found or moved.");
+                return new();
+            }
+        }
+    }
+}
