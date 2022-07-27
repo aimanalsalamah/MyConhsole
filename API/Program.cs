4392338ff78dd9f9f2a2bc2ac03b36f79d4296e9
mohammed_988 <m7md_alodh@hotmail.com>
2022-07-27 09:49:45 +0300

    ipa users

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+app.UseCors(p => p.AllowAnyOrigin());
 app.UseHttpsRedirection();
 
 app.MapGet("/home", () =>
