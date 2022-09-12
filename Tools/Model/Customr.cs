18a6fee4321f4898979bf3c6346c4e6af9c46d00
fahad almalki <fahadw835@gmail.com>
2022-09-12 10:25:58 +0300

    Add new disgin button

--- Tools/Model/Customr.cs
+++ Tools/Model/Customr.cs
@@ -1,10 +1,14 @@
-﻿namespace Tools.Model
+﻿using System.Data;
+
+namespace Tools.Model
 {
     public class Customr
     {
-        public int id { get; set; }
+       
         public string name { get; set; }
+        public int id { get; set; }
         public int Age { get; set; }
+
 
     }
 }
